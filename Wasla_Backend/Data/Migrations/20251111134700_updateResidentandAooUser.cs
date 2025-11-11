4f7aff2d0e56ffd384649a5cb5d74f6cfdd6931b
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-11-11 15:52:57 +0200

    Update DB

---
+++ Wasla_Backend/Data/Migrations/20251111134700_updateResidentandAooUser.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wasla_Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateResidentandAooUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Restaurant");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Restaurant");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Resident");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Resident");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Gym");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Gym");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Driver");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Driver");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Doctor");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Doctor");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.CreateTable(
+                name: "residentIdentities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NationalId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_residentIdentities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "residentIdentities");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Restaurant",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Restaurant",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Resident",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Resident",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Gym",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Gym",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Driver",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Driver",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Doctor",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Doctor",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
