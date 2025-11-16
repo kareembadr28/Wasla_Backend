0161cd7de99edf245bc1f9ab9960dbfe0790ad3f
kareembadr28 <bdrkrym558@gmail.com>
2025-11-16 19:05:31 +0200

    migrations

---
+++ Wasla_Backend/Data/Migrations/20251116170352_updaterestaurant.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wasla_Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updaterestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerName",
+                table: "Restaurant",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerName",
+                table: "Gym",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerName",
+                table: "Driver",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerName",
+                table: "Doctor",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerName",
+                table: "Restaurant");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerName",
+                table: "Gym");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerName",
+                table: "Driver");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerName",
+                table: "Doctor");
+        }
+    }
+}
